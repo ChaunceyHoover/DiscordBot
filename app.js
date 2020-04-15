5770eb923151e2209e16865e34d1423ca33e1341
Chauncey Hoover <dealernextdoor@gmail.com>
2020-04-14 22:33:58 -0400

    Added documentation to app.js

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
 
+// Create discord.js objects
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
+client.commands = new Discord.Collection(); // store commands instead of writing if statement for each command
 
 // Load in commands dynamically
 const commandFiles = fs.readdirSync('./commands');
 
+// Adds each command in the `./commands` folder to the bot
 for (const file of commandFiles) {
 	if (file !== "cmd.tmpl.js") {
 		const cmd = require(`./commands/${file}`);
@@ -15,16 +17,21 @@ for (const file of commandFiles) {
 	}
 }
 
+// Basically the 'onStart' method - this runs when it successfully connects to discord and initiates itself
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}.`);
 });
 
+// onMessage - Runs every time someone sends a message
+// quick reference `msg`: https://discord.js.org/#/docs/main/stable/class/Message
 client.on('message', msg => {
-	if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+	if (!msg.content.startsWith(prefix) || msg.author.bot) return; // if message doesn't start with command prefix or is from bot, ignore
 
+	// Split command into an array (ex: "!ping hello world" => ['!ping', 'hello', 'world'])
 	const args = msg.content.slice(prefix.length).split(/ +/);
 	const cmd = args.shift().toLowerCase();
 
+	// The only built in command - !cmds / !commands. Lists all registered commands w/ a description
 	if (cmd === "commands" || cmd === "cmds") {
 		var cmds = client.commands.values();
 		var bot_reply = "\n";
@@ -35,8 +42,10 @@ client.on('message', msg => {
 		return;
 	}
 
+	// Ignores if they try to run a command bot doesn't have
 	if (!client.commands.has(cmd)) return;
 
+	// Command was found, so run it and report any errors.
 	try {
 		client.commands.get(cmd).execute(msg, args);
 	} catch(err) {
@@ -45,4 +54,5 @@ client.on('message', msg => {
 	}
 });
 
+// Starts the bot
 client.login(token);
