0e0acbd3e1ef5a2741766f903a0e042378709b40
Chauncey Hoover <dealernextdoor@gmail.com>
2020-12-04 18:54:55 -0500

    Initial commit
    Added very basic web app hosted through express.

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+// Discord.js imports
 const fs = require('fs');
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const { port, prefix, token } = require('./config.json');
+
+// Web app
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 // Create discord.js objects
 const client = new Discord.Client();
@@ -57,3 +63,25 @@ client.on('message', msg => {
 
 // Starts the bot
 client.login(token);
+
+// Start web app
+const server = express();
+const port = port || 3000;
+
+// Set view engine to pug
+server.set('views', path.join(__dirname, 'views'));
+server.set('view engine', 'pug');
+
+// Set root directory for web server
+server.use(express.static(path.join(__dirname, 'wwwroot')));
+
+// Process requests as JSON
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+
+// Process URLs
+server.use(require('./routes/index')); // allows for custom URLs & removal of file extensions
+server.use('/api', require('./routes/api')); // maps all API calls to /api/*
+
+server.listen(port);
+console.log(`Successfully started on port ${port}`);
